91a16c3f861bef1e68a518325b0464a59f44d8d2
Miguel Muñoz <SwingGuy1024@yahoo.com>
2018-07-31 18:32:30 -0400

    Create safeSortedMap.java

---
+++ src/main/java/com/mm/exp/util/safeSortedMap.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright © [2018] Common Securitization Solutions, LLC. All rights reserved.
+ * This software contains confidential information and trade secrets of Common Securitization
+ * Solutions, LLC. Use, disclosure, or reproduction is prohibited without the prior written
+ * consent of Common Securitization Solutions, LLC.
+ */
+
+package com.mm.exp.util;
+
+import java.util.SortedMap;
+
+public interface SafeSortedMap<K, V> extends SortedMap<K, V> {
+}
