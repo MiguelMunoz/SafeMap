2c7e4dfb10c6c099aac98b4b4881ccda65a00df7
Miguel Munoz <SwingGuy1024@yahoo.com>
2018-08-19 13:11:32 -0700

    comments

--- src/main/java/com/mm/exp/extend/SafeSortedMap.java
+++ src/main/java/com/mm/exp/extend/SafeSortedMap.java
@@ -1,10 +1,3 @@
-/*
- * Copyright © [2018] Common Securitization Solutions, LLC. All rights reserved.
- * This software contains confidential information and trade secrets of Common Securitization
- * Solutions, LLC. Use, disclosure, or reproduction is prohibited without the prior written
- * consent of Common Securitization Solutions, LLC.
- */
-
 package com.mm.exp.extend;
 
 import java.util.SortedMap;
